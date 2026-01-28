413067571ca3c35b205cf53df2dd234b08981c89
matheuspuzzi <99302349+matheuspuzzi@users.noreply.github.com>
2026-01-28 14:46:16 -0300

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE="ficha-v1";
+self.addEventListener("install",e=>{
+ e.waitUntil(caches.open(CACHE).then(c=>c.addAll([
+  "./","./index.html","./admin.html","./manifest.json"
+ ])));
+});
+self.addEventListener("fetch",e=>{
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
